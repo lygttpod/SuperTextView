e11186a1d8c6c0da5d6b96193b48b1ac73ea9644
Allen <lygttpod@163.com>
2019-09-27 18:14:18 +0800

    去除阴影代码

--- library/src/main/java/com/allen/library/shape/ShapeButton.kt
+++ library/src/main/java/com/allen/library/shape/ShapeButton.kt
@@ -1,12 +1,10 @@
 package com.allen.library.shape
 
 import android.content.Context
-import android.graphics.Canvas
 import android.support.v7.widget.AppCompatButton
 import android.util.AttributeSet
 import com.allen.library.data.AttributeSetData
 import com.allen.library.helper.AttributeSetHelper
-import com.allen.library.helper.ShadowHelper
 import com.allen.library.helper.ShapeBuilder
 
 /**
@@ -19,29 +17,11 @@ import com.allen.library.helper.ShapeBuilder
  */
 class ShapeButton @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : AppCompatButton(context, attrs, defStyleAttr) {
     var shapeBuilder: ShapeBuilder? = null
-    var shadowHelper: ShadowHelper? = null
-
     var attributeSetData: AttributeSetData = AttributeSetData()
 
     init {
         attributeSetData = AttributeSetHelper().loadFromAttributeSet(context, attrs)
-        if (attributeSetData.showShadow) {
-            shadowHelper = ShadowHelper()
-            shadowHelper?.init(this, attributeSetData)
-        } else {
-            shapeBuilder = ShapeBuilder()
-            shapeBuilder?.init(this, attributeSetData)
-        }
-    }
-
-    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        super.onSizeChanged(w, h, oldw, oldh)
-        shadowHelper?.onSizeChanged(w, h)
-    }
-
-    override fun draw(canvas: Canvas) {
-        shadowHelper?.drawBefore(canvas)
-        super.draw(canvas)
-        shadowHelper?.drawOver(canvas)
+        shapeBuilder = ShapeBuilder()
+        shapeBuilder?.init(this, attributeSetData)
     }
 }
